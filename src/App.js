c5d13ccc2bd4c89b9064ef2568b66d843bfdd5b5
Kutman <frontdevkg@gmail.com>
2021-07-28 20:29:01 +0600

    commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,96 @@
-import React, { Component } from 'react'
+import React, {useState, useEffect, useReducer} from 'react'
 import TodoList from './TodoList'
+import {Context} from './context'
+import reducer from "./reducer";
 
-export default class App extends Component {
-  state = {
-    todos: [
-      {id: 1, title: 'First todo', completed: false},
-      {id: 2, title: 'Second todo', completed: true},
-    ]
-  }
+export default function App() {
+    // Объявляем новую переменную состояния "count"
+    const [count, setCount] = useState(0);
+    useEffect(() => {
+        // Обновляем заголовок документа, используя API браузера
+        document.title = `Вы нажали ${count} раз`;
+    });
 
-  render() {
+
+    const [state, dispatch] = useReducer(reducer, JSON.parse(localStorage.getItem('todos')))
+    // const [todos, setTodos] = useState([])
+    // const [todos, setTodos] = useState([
+    //     {id: 1, title: 'First todo', completed: false},
+    //     {id: 2, title: 'Second todo', completed: true},
+    // ])
+    // state = {
+    //   todos: [
+    //     {id: 1, title: 'First todo', completed: false},
+    //     {id: 2, title: 'Second todo', completed: true},
+    //   ]
+    // }
+    // useEffect(() => {
+    //     const raw = localStorage.getItem('todos') || []
+    //     setTodos(JSON.parse(raw))
+    // }, [])
+    useEffect(() => {
+        localStorage.setItem('todos', JSON.stringify(state))
+    }, [state])
+
+    const [todoTitle, setTodoTitle] = useState('')
+
+    const addTodo = e => {
+        if (e.key === 'Enter') {
+            dispatch({
+                type: 'add',
+                payload: todoTitle
+            })
+            dispatch([...state,
+                {
+                    id: Date.now(),
+                    title: todoTitle,
+                    completed: false
+                }])
+            setTodoTitle('')
+        }
+    }
+    // const removeTodo = id => {
+    //     setTodos(todos.filter(todo => {
+    //         return todo.id = !id
+    //     }))
+    // }
+    // const toglleTodo = id => {
+    //     setTodos(todos.map(todo => {
+    //         if (todo.id === id) {
+    //             todo.completed = !todo.completed
+    //         }
+    //         return todo
+    //     }))
+    // }
+
     return (
-      <div className="container">
-        <h1>Todo app</h1>
+        <Context.Provider value={{
+            dispatch
+            // toglleTodo, removeTodo
+        }}>
+            <div className="container">
+                <h1>Todo app</h1>
 
-          <div className="input-field">
-            <input type="text" />
-            <label>Todo name</label>
-          </div>
+                <div className="input-field">
+                    <input type="text"
+                           value={todoTitle}
+                           onChange={e => {
+                               setTodoTitle(e.target.value)
+                           }}
+                           onKeyPress={addTodo}
+                    />
+                    <label>Todo name</label>
+                </div>
 
-          <TodoList todos={this.state.todos} />
-      </div>
+                <TodoList todos={state}/>
+                <div>
+                    <p>Вы нажали {count} раз</p>
+                    <button onClick={() => setCount(count + 1)}>
+                        Нажми на меня
+                    </button>
+                </div>
+            </div>
+        </Context.Provider>
     );
-  }
+
 }
--- src/TodoItem.js
+++ src/TodoItem.js
@@ -1,17 +1,33 @@
-import React from 'react'
+import React,{useState,useContext} from 'react'
+import {Context} from "./context";
 
 export default function TodoItem({title, id, completed}) {
+  // const [checked,setChecked] = useState(completed)
+  const {dispatch} = useContext(Context)
+
+  const clas = ['todo']
+  if (completed){
+    clas.push('completed')
+  }
   return (
-    <li className="todo">
+    <li className={clas.join(' ')}>
       <label>
         <input
           type="checkbox"
-          defaultChecked={false}
+          checked={completed}
+          onChange={()=>dispatch({
+              type: 'toggle',
+              payload: id
+          })}
         />
         <span>{title}</span>
 
         <i
           className="material-icons red-text"
+          onClick={()=>dispatch({
+            type: 'remove',
+            payload:id
+          })}
         >
           delete
         </i>
